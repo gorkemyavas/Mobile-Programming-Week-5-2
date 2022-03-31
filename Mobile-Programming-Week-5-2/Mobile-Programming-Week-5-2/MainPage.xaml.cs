a3854e4ac67bf7cf9334e7f44093162d7be3545a
gorkem <gorkemyavas98@gmail.com>
2022-03-31 10:25:01 +0300

    Add project files.

---
+++ Mobile-Programming-Week-5-2/Mobile-Programming-Week-5-2/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Mobile_Programming_Week_5_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new BoxView
+            {
+                Color = Color.Blue,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                WidthRequest = 200,
+                HeightRequest = 100,
+                CornerRadius = 5
+            };
+        }
+    }
+}
